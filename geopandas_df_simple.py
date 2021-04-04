8310243b7b9bea00d896ada9787455da72c1a01c
teo113 <faull.theo@gmail.com>
2021-04-05 00:57:11 +0100

    added geopandas and dtype info

--- geopandas_df_simple.py
+++ geopandas_df_simple.py
@@ -1,8 +1,10 @@
 # author: github.com/teo113
-# desc: This script utilises the pandas module for various tasks.
+# desc: This script loads a geopandas dataframe from file and plots the data.
 
 import pandas as pd
-import numpy as np
+import geopandas
+import matplotlib.pyplot as plt
+#import pyproj
 
 csv_file = r'data/pandas_locs.csv'
 
@@ -16,6 +18,25 @@ df = pd.read_csv(f'{csv_file}', index_col='id', dtype={'id': 'uint32',
                                                        'srid': 'uint16'
                                                        })
 print(df)
-# replace NaN values with empty strings
-df1 = df.replace(np.nan, '', regex=True)
-print(df1)
+
+# create geodataframe
+gdf = geopandas.GeoDataFrame(
+    df, geometry=geopandas.points_from_xy(df.xco, df.yco), crs='EPSG:27700')
+print(gdf.head())
+
+# define projections OLD?
+#wgs84 = pyproj.Proj(init='epsg:4326')
+#bng = pyproj.Proj(init='epsg:27700')
+
+# get country boundary data
+gb_map = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+gb_map.crs
+gb_map = gb_map.to_crs('EPSG:27700')
+
+# restrict map to UK
+ax = gb_map[gb_map.name == 'United Kingdom'].plot(
+    color='white', edgecolor='black')
+
+# plot map
+gdf.plot(ax=ax, color='red')
+plot.show()
